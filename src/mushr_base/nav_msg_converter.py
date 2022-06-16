8223b9e6a04c8d56a0c28348a423a6c52ffec1a2
mcdoerr <mcdoerr@uw.edu>
2022-06-16 13:53:43 -0700

    update converter default behaviors and params

--- src/mushr_base/nav_msg_converter.py
+++ src/mushr_base/nav_msg_converter.py
@@ -16,12 +16,11 @@ class NavMsgConverter:
     Attributes:
         name (string) rosnode name
     """
-    self.pose = None
     self.type = None
 
     # Create the subscribers
     self.pose_sub = rospy.Subscriber(
-      rospy.get_param("~pose_topic"), PoseStamped, self.save_pose, queue_size=100
+      rospy.get_param("~pose_topic"), PoseStamped, self.publish_pose, queue_size=100
     )
     self.type_sub = rospy.Subscriber(
       rospy.get_param("~type_topic"), String, self.save_type, queue_size=100
@@ -32,25 +31,21 @@ class NavMsgConverter:
     self.car_pose_pub = rospy.Publisher(rospy.get_param("~start_topic"), PoseStamped, queue_size=1)
     self.pose_estimate_pub = rospy.Publisher(rospy.get_param("~estimate_topic"), PoseStamped, queue_size=1)
 
-  def save_pose(self, pose_msg: PoseStamped) -> None:
+  def publish_pose(self, pose_msg: PoseStamped) -> None:
     """
     Take a pose stamped message and save it.
     """
-    self.pose = pose_msg
+    print("publishing")
+    if self.type == 'goal':
+      self.goal_pub.publish(pose_msg)
+    elif self.type == 'estimate':
+      self.pose_estimate_pub.publish(pose_msg)
+    else:
+      self.car_pose_pub.publish(pose_msg)
   
   def save_type(self, type_msg: String) -> None:
     self.type = type_msg.data
+    print("type: " + str(self.type))
     if self.type != 'pose' and self.type != 'goal' and self.type != 'estimate':
       raise Exception(f'Invalid type detected {self.type}')
-    if self.pose is not None:
-        self.publish()
-  
-  def publish(self) -> None:
-    if self.type == 'pose':
-      self.car_pose_pub.publish(self.pose)
-    elif self.type == 'estimate':
-      self.pose_estimate_pub.publish(self.pose)
-    else:
-      self.goal_pub.publish(self.pose)
-    self.pose = None
-    self.type = None
+
